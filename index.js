c797c90800b371d2a425ff6c3b8265ddbb3c5e82
Jesse Crocker <jesse@DataMongers.net>
2015-05-07 06:17:47 -0600

    Set response content type

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.contentType('application/json');
     next();
 });
 app.post('/geojson', function(req, res) {
