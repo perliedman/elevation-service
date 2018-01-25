59f2199c3b4fa4ff12d4e68e20fec83c2261ea5c
Per Liedman <per.liedman@dotnetmentor.se>
2018-01-25 14:58:04 +0100

    Refactor to micro and node 8

--- index.js
+++ index.js
@@ -1,70 +1,32 @@
-var addElevation = require('geojson-elevation').addElevation,
-    TileSet = require('node-hgt').TileSet,
-    ImagicoElevationDownloader = require('node-hgt').ImagicoElevationDownloader,
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    app = express(),
-    tileDirectory = process.env.TILE_DIRECTORY,
-    tiles,
-    tileDownloader,
-    noData;
+const {json, send} = require('micro')
+const addElevation = require('geojson-elevation').addElevation
+const TileSet = require('node-hgt').TileSet
+const ImagicoElevationDownloader = require('node-hgt').ImagicoElevationDownloader
+const tileDirectory = process.env.TILE_DIRECTORY || './data'
+const noData = process.env.NO_DATA ? parseInt(process.env.NO_DATA) : null
+const tileDownloader = process.env.TILE_DOWNLOADER === 'none'
+  ? tileDownloader = undefined
+  : new ImagicoElevationDownloader(tileDirectory)
+const maxPostSize = process.env.MAX_POST_SIZE || "500kb";
+const tiles = new TileSet(tileDirectory, {downloader:tileDownloader});
 
-if (!tileDirectory) {
-    tileDirectory = './data';
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    return send(res, 405, {error: 'Only POST allowed'})
+  }
+
+  const geojson = await json(req, {limit: maxPostSize})
+  if (!geojson || Object.keys(geojson).length === 0) {
+    return send(res, 400, {error: 'invalid GeoJSON'})
+  }
+
+  return new Promise((resolve, reject) => {
+    addElevation(geojson, tiles, function(err, geojson) {
+      if (err) {
+        return send(res, 500, err)
+      }
+
+      resolve(geojson)
+    }, noData)
+  })
 }
-
-if (!process.env.TILE_DOWNLOADER || process.env.TILE_DOWNLOADER === 'imagico') {
-    tileDownloader = new ImagicoElevationDownloader(tileDirectory);
-} else if(process.env.TILE_DOWNLOADER === 'none') {
-    tileDownloader = undefined;
-}
-
-if (process.env.NO_DATA) {
-    noData = parseInt(process.env.NO_DATA);
-}
-
-tiles = new TileSet(tileDirectory, {downloader:tileDownloader});
-
-var maxPostSize = "500kb";
-if (process.env.MAX_POST_SIZE) {
-    maxPostSize = process.env.MAX_POST_SIZE;
-}
-
-app.use(bodyParser.json({limit: maxPostSize}));
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.contentType('application/json');
-    next();
-});
-app.post('/geojson', function(req, res) {
-    var geojson = req.body;
-
-    if (!geojson || Object.keys(geojson).length === 0) {
-        res.status(400).send('Error: invalid geojson.');
-        return;
-    }
-
-    addElevation(geojson, tiles, function(err) {
-        if (err) {
-            var errMsg = JSON.stringify(err);
-            process.stderr.write(errMsg + '\n');
-            res.status(500).send(errMsg);
-        } else {
-            res.send(JSON.stringify(geojson));
-        }
-    }, noData);
-});
-
-app.get('/status', function(req, res) {
-    res.send();
-});
-
-var server = app.listen(5001, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('elevation-server listening at http://%s:%s', host, port);
-});
-
-module.exports = app;
--- test/spec.js
+++ test/spec.js
@@ -1,28 +1,26 @@
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var should = chai.should();
-var app = require('../');
+const micro = require('micro')
+const test = require('ava')
+const listen = require('test-listen')
+const request = require('request-promise-native')
+var app = require('../')
 
-chai.use(chaiHttp);
+test('should add elevation data for point', async t => {
+  const service = micro(app)
+  const uri = await listen(service)
+  const body = await request({
+    uri,
+    method: 'POST',
+    body: {
+      type: 'Point',
+      coordinates: [11.9, 57.7]
+    },
+    json: true
+  })
 
-describe('routes', function() {
-    describe('POST /geojson', function() {
-        it('should add elevation data for point', function(done) {
-            chai
-                .request(app)
-                .post('/geojson')
-                .send({
-                    type: 'Point',
-                    coordinates: [11.9, 57.7]
-                })
-                .end(function(err, res) {
-                    should.not.exist(err);
-                    res.status.should.equal(200);
-                    res.type.should.equal('application/json');
-                    should.exist(res.body.coordinates);
-                    res.body.coordinates.length.should.equal(3);
-                    done();
-                });
-        });
-    });
+  t.is(body.type, 'Point')
+  t.truthy(body.coordinates)
+  t.is(body.coordinates.length, 3)
+  t.true(Math.abs(body.coordinates[2] - 13) < 1e-3)
+
+  service.close()
 });
